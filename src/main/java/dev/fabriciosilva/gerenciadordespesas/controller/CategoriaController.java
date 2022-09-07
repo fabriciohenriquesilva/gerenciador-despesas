ab0dc3c4a498adf437728e05c2c03c748667035c
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2022-09-06 22:08:41 -0300

    feat: page to see list of categoria

---
+++ src/main/java/dev/fabriciosilva/gerenciadordespesas/controller/CategoriaController.java
@@ -0,0 +1,27 @@
+package dev.fabriciosilva.gerenciadordespesas.controller;
+
+import dev.fabriciosilva.gerenciadordespesas.domain.Categoria;
+import dev.fabriciosilva.gerenciadordespesas.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping
+    public String listaCategorias(Model model){
+        List<Categoria> categorias = categoriaService.listarTodos();
+        model.addAttribute("categorias", categorias);
+        return "categoria/listaCategorias";
+    }
+
+}
---
+++ src/main/java/dev/fabriciosilva/gerenciadordespesas/service/CategoriaService.java
@@ -0,0 +1,19 @@
+package dev.fabriciosilva.gerenciadordespesas.service;
+
+import dev.fabriciosilva.gerenciadordespesas.domain.Categoria;
+import dev.fabriciosilva.gerenciadordespesas.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public List<Categoria> listarTodos(){
+        return categoriaRepository.findAll();
+    }
+}
