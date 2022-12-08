2b1fe77679b9fb799a69efb2440d46885fe0d653
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2022-12-08 17:21:14 -0300

    feat: credor and subcategoria for user input on forms

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,15 @@
+let credorIdInput = document.querySelector('#credor-id');
+credorIdInput.addEventListener('blur', () => preencherNome(credorIdInput.value));
+let credorNomeInput = document.querySelector('#credor-nome');
+
+function preencherNome(id) {
+    fetch(`http://localhost:8080/api/pessoa/${id}`)
+        .then(res => res.json())
+        .then(res => {
+            if(res.nome !== undefined) {
+                credorNomeInput.value = res.nome;
+            } else {
+                credorNomeInput.value = '';
+            }
+        });
+}
