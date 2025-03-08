c60bcf0cff26d3af205023dc6f3a79de45ab7e06
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2025-03-08 14:40:58 -0300

    feat: new method to get user instance on application

--- src/main/java/dev/fabriciosilva/controlefinanceiro/infra/context/AuthenticationFacade.java
+++ src/main/java/dev/fabriciosilva/controlefinanceiro/infra/context/AuthenticationFacade.java
@@ -1,9 +1,12 @@
 package dev.fabriciosilva.controlefinanceiro.infra.context;
 
+import dev.fabriciosilva.controlefinanceiro.domain.user.User;
 import org.springframework.security.core.Authentication;
 
 public interface AuthenticationFacade {
 
     Authentication getAuthentication();
+
+    User getUser();
 
 }
--- src/main/java/dev/fabriciosilva/controlefinanceiro/infra/context/AuthenticationFacadeImpl.java
+++ src/main/java/dev/fabriciosilva/controlefinanceiro/infra/context/AuthenticationFacadeImpl.java
@@ -1,5 +1,6 @@
 package dev.fabriciosilva.controlefinanceiro.infra.context;
 
+import dev.fabriciosilva.controlefinanceiro.domain.user.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -10,5 +11,16 @@ public class AuthenticationFacadeImpl implements AuthenticationFacade {
     @Override
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @Override
+    public User getUser() {
+        Object principal = getAuthentication().getPrincipal();
+
+        if (principal instanceof User) {
+            return (User) principal;
+        }
+
+        return null;
     }
 }
